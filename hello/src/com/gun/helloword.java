eee873154c2af1cb87b790b004e4895db21d1d41
huangdagang <1045149620@qq.com>
2020-07-20 13:56:41 +0800

    提交

---
+++ hello/src/com/gun/helloword.java
@@ -0,0 +1,9 @@
+package com.gun;
+
+public class helloword {
+    public static void main(String args[]){
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+    }
+}
